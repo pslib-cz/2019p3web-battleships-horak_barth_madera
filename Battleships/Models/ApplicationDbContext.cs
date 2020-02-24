b9e0de20578b81473e5f3eda7c0bbbad7ab5bd25
Michal Barth <u17.401@pslib.cloud>
2020-02-24 13:16:22 +0100

    loď

---
+++ Battleships/Models/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Battleships.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Ship> ship { get; set; }
+        public ApplicationDbContext() : base()
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BattleshipsDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ship>().HasData(new Ship { Id = 1, Name = "Konstantinův křižník." });
+        }
+    }
+}
--- Battleships/Models/Ship.cs
+++ Battleships/Models/Ship.cs
@@ -7,5 +7,7 @@ namespace Battleships.Models
 {
     public class Ship
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
