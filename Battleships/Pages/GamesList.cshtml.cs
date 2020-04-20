0fd57223cf21eeee44c53726d8ca88394d0b88e2
Michal Barth <u17.401@pslib.cloud>
2020-04-20 10:45:32 +0200

    vypsání existujících her

--- Battleships/Pages/GamesList.cshtml.cs
+++ Battleships/Pages/GamesList.cshtml.cs
@@ -6,6 +6,7 @@ using Battleships.Models;
 using Battleships.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Utf8Json;
 
 namespace Battleships.Pages
 {
@@ -22,12 +23,24 @@ namespace Battleships.Pages
             _session = session;
         }
 
+        private List<Game> LoadGames()
+        {
+            return (from p in _db.Games /*orderby Guid.NewGuid()*/ select p).Take(10).ToList();
+        }
+
         public void OnGet()
         {
-            foreach (var item in _db.Games)
+            _session.Save("GamesList", JsonSerializer.ToJsonString(LoadGames()));
+            for (int i = 0; i <= _db.Games.Count(); i++)
             {
-                Games.Add(item);
+                Games = JsonSerializer.Deserialize<List<Game>>(_session.Load("GamesList"));
             }
+        }
+
+        private ActionResult OnGetEnterGame(Guid value)
+        {
+            //načíst data dané hry
+            return RedirectToPage("./GamePreparation");
         }
     }
 }
