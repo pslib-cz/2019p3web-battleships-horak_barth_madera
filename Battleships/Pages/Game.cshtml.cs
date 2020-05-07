1c06bd2ed2553bb0bb4ca4753abcef4b3047c48a
Petr Horák <petr.horak@pslib.cz>
2020-05-07 12:48:17 +0200

    Úprava partial - nefunkční

--- Battleships/Pages/Game.cshtml.cs
+++ Battleships/Pages/Game.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Battleships.Models;
 using Battleships.Services;
+using Battleships.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -13,24 +14,27 @@ namespace Battleships.Pages
     {
         private IGame _gameService;
         private SessionStorage<Guid> _sessionGuid;
-        
+        public Game Game;
         public Guid _gameId;
         public int _gameSize;
-
+        public BattlefieldPartialModel partialModel;
         public List<List<NavyBattlePiece>> PlayingField;
         
 
-        public GameModel(IGame game, SessionStorage<Guid> sessionGuid)
-        {
+        public GameModel(IGame game, SessionStorage<Guid> sessionGuid, BattlefieldPartialModel partial)
+        {           
             _gameService = game;
             _sessionGuid = sessionGuid;            
-            _gameId = sessionGuid.Load("GameId");                     
+            _gameId = sessionGuid.Load("GameId");
+            partialModel = partial;
         }
 
         public void OnGet()
         {
             PlayingField = _gameService.GetBattlefield(_gameId);
-            
+            Game = _gameService.GetGame(_gameId);
+
+            partialModel._battlefield = PlayingField;
         }
     }
 }
---
+++ Battleships/ViewModels/BattlefieldPartialModel.cs
@@ -0,0 +1,18 @@
+﻿using Battleships.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Battleships.ViewModels
+{
+    public class BattlefieldPartialModel
+    {
+        public List<List<NavyBattlePiece>> _battlefield;
+
+        public BattlefieldPartialModel(List<List<NavyBattlePiece>> bf)
+        {
+            _battlefield = bf;
+        }
+    }
+}
